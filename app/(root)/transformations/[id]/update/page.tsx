d40233e04535878b8d32e1aa08428283abb364c3
vipul <vipulgokujain@gmail.com>
2024-04-18 21:44:59 +0530

    initial commit

---
+++ app/(root)/transformations/[id]/update/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UpdatingTransformations = () => {
+  return (
+    <div>
+      Updating Transformations Page
+    </div>
+  )
+}
+
+export default UpdatingTransformations
